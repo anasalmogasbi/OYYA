833a4b410e470039bff062d77b054cc737482431
anasalmogasbi <anas_almogasbi@yahoo.com>
2026-09-02 06:02:31 +0200

    Keep OYYA system account outside 20-user test capacity

--- public/entry.php
+++ public/entry.php
@@ -6,6 +6,7 @@ $bad="foreach(['skills'=>'مهارات','interests'=>'اهتمامات','study_w
 $good="foreach(['skills'=>'مهارات','interests'=>'اهتمامات','study_work'=>'دراسة/عمل','offers'=>'أقدم','seeks'=>'أبحث'] as \$k=>\$lab):if(!empty(\$u[\$k])):?><span class=\"chip\"><?=\$lab?>: <?=h((string)\$u[\$k])?></span><?php endif;endforeach;?>";
 $source=str_replace($bad,$good,$source,$count);
 if($count!==1){http_response_code(500);echo 'OYYA compile guard failed.';exit;}
+$source=str_replace('$usersCount=count($users);','$usersCount=count(oyya_test_users());',$source);
 $source=str_replace('__DIR__',var_export(__DIR__,true),$source);
 $tmp=sys_get_temp_dir().'/oyya-main-'.sha1($source).'.php';
 file_put_contents($tmp,$source,LOCK_EX);
